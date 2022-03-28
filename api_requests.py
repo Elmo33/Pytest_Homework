fce8575b6d7f5c86ff6b9659278d270cb1934b62
Saba Momtselidze <saba.momtselidze@exactprosystems.com>
2022-03-28 22:21:27 +0400

    used parametrize

--- api_requests.py
+++ api_requests.py
@@ -16,23 +16,21 @@ class API:
             r = requests.post(self.url, json=payload, headers=self.headers)
             if 'error' not in r.json():
                 self.bot_id = int(r.json()["id"])
-        return r
+        return {"status_code": r.status_code, "json": r.json()}
 
     def get_request(self, reqs):
         attributes = "".join([f"{i}={reqs[i]}&" for i in reqs.keys()])[:-1]
         r = requests.get(f"{self.url}?{attributes}", headers=self.headers)
-        return r
+        return {"status_code": r.status_code, "json": r.json()}
 
     def patch_request(self, bot_id, payload):
         r = requests.patch(f"{self.url}?bot_id={bot_id}", json=payload, headers=self.headers)
-        return r
+        return {"status_code": r.status_code, "json": r.json()}
 
     def put_request(self, bot_id, payload):
         r = requests.put(f"{self.url}?bot_id={bot_id}", json=payload, headers=self.headers)
-        return r
+        return {"status_code": r.status_code, "json": r.json()}
 
     def delete_request(self, bot_id):
         r = requests.delete(f"{self.url}?bot_id={bot_id}", headers=self.headers)
-        return r
-
-
+        return {"status_code": r.status_code, "json": r.text}
---
+++ test_cases.py
@@ -0,0 +1,68 @@
+import json
+import pytest
+from api_requests import API
+
+
+# execute with pytest -s --html=report.html
+
+@pytest.fixture()
+def demo_message():
+    print("\nStarting to launch the test case")
+    print("----------------------------------")
+    yield
+    print("Ending the test case")
+
+
+headers = {"Authorization": json.dumps({"token": "sup3rs3cr3t"})}
+headers2 = {"Authorization": json.dumps({"username": "username", "password": "password"})}
+headers3 = {"Authorization": json.dumps({"token": "wrong"})}
+headers4 = {"Authorization": json.dumps({"username": "username2", "password": "passw2ord"})}
+
+first = API("http://127.0.0.1:5000/", headers)
+second = API("http://127.0.0.1:5000/", headers2)
+third = API("http://127.0.0.1:5000/", headers3)
+fourth = API("http://127.0.0.1:5000/", headers4)
+
+
+payload1 = {"url": "http://example.com"}
+payload2 = {"url": "http://example.com4362375"}
+payload3 = {"intents": ["play-sound", "tell-joke"]}
+
+
+def test_get_request(demo_message):
+    result = first.get_request({"bot_id": first.bot_id})
+    assert result.status_code == 200
+
+
+# THIS FUNCTION IS SO COOOOOOOOOOOOOOOOOOOOOOL, feel like a primate now for writing test cases separately
+@pytest.mark.parametrize(
+    ('input_x', 'code', 'expected'),
+    (
+            pytest.param(first.get_request({"bot_id": first.bot_id}), 200, None, id="First get request"),
+            pytest.param(first.post_request(payload1), 200, {'id': first.bot_id, 'intents': [], 'name': 'test', 'url': 'http://example.com'},
+                         id="First post request"),
+            pytest.param(first.patch_request(first.bot_id, payload2), 200, None, id="First patch request"),
+            pytest.param(first.put_request(first.bot_id, payload3), 200, None, id="First put request"),
+            pytest.param(first.delete_request(first.bot_id), 200, None, id="First delete request"),
+            pytest.param(second.get_request({"bot_id": first.bot_id}), 200, None, id="Second get request"),
+            pytest.param(second.post_request(payload1), 200, {'id': first.bot_id, 'intents': [], 'name': 'test', 'url': 'http://example.com'},
+                         id="Second post request"),
+            pytest.param(second.patch_request(first.bot_id, payload2), 200, None, id="Second patch request"),
+            pytest.param(second.put_request(first.bot_id, payload3), 200, None, id="Second put request"),
+            pytest.param(second.delete_request(first.bot_id), 200, None, id="Second delete request"),
+
+            pytest.param(first.post_request("a"), 200, {'error': 'Content-Type not supported!'},
+                         id="Post with invalid data"),
+            pytest.param(first.post_request(), 200, {'error': 'Content-Type not supported!'},
+                         id="Post with no data"),
+            pytest.param(third.post_request(payload1), 200, {"error": "wrong token code"},
+                         id="Post with wrong secret"),
+            pytest.param(fourth.post_request(payload1), 200, {"error": "wrong credentials"},
+                         id="Post with wrong credentials"),
+    )
+)
+def test_requests(input_x: dict, code, expected):
+    result = input_x
+    if expected:
+        assert result["json"] == expected
+    assert result["status_code"] == code
