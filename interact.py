6de0f7e43ea169b2b82f1c08ae34e7afddd6c9e0
Saba Momtselidze <saba.momtselidze@exactprosystems.com>
2022-03-29 10:03:39 +0400

    finished, hopefully

--- interact.py
+++ interact.py
@@ -8,7 +8,7 @@ first = API("http://127.0.0.1:5000/", headers)
 
 def get_request(intent, answer, step_id):
     result = first.get_request({"intent": intent, "step": step_id, "answer": answer})
-    print(result["json"]["next"])
+    print(result["json"]["text"])
     return int(result["json"]["next"])
 
 
